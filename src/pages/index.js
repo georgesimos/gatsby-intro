f8631d581bd04ea0f0fc5e877e6a61454e65935b
George <admin@georgesimos.com>
2020-02-01 18:37:03 +0200

    Styling the Main Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello Gatsby</p>
-    <Link to="/">Learn about me &rarr;</Link>
-  </>
+    <Link to="/about">Learn about me &rarr;</Link>
+  </Layout>
 );
