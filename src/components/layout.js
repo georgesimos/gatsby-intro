c87770640ad04dde1e13ce6881a186fe7dbc62d7
George <admin@georgesimos.com>
2020-02-05 20:52:20 +0200

    layout corrections

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { Global, css } from '@emotion/core';
 import Helmet from 'react-helmet';
 import Header from './header';
 import useSiteMetadata from '../hooks/use-sitemetadata';
+
 const Layout = ({ children }) => {
   const { title, description } = useSiteMetadata();
 
@@ -14,53 +15,54 @@ const Layout = ({ children }) => {
             box-sizing: border-box;
             margin: 0;
           }
-
+          /* More info: https://bit.ly/2PsCnzk */
           * + * {
             margin-top: 1rem;
           }
-
-          html,body {
-              margin: 0;
-              color: #555;
-              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
-              font-size: 18px;
-              line-height; 1.4;
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+              'Segoe UI Emoji', 'Segoe UI Symbol';
+            font-size: 18px;
+            line-height: 1.4;
+            /* remove margin for the main div that Gatsby mounts into */
+            > div {
+              margin-top: 0;
+            }
           }
-
-          /* Remove margin from the main div #___gatsby */
-          > div {
-            margin-top: 0;
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+            + * {
+              margin-top: 0.5rem;
+            }
           }
-
-          h1,h2,h3,h4,h5,h6 {
-              color: #222;
-              line-height: 1.1;
-
-              + * {
-                  margin-top: 0.5rem;
-              }
-          }
-
           strong {
-              color: #222;
+            color: #222;
           }
-
           li {
-              margin-top: 0.25rem;
+            margin-top: 0.25rem;
           }
         `}
       />
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
-        <meta name={description} content="site description" />
+        <meta name="description" content={description} />
       </Helmet>
       <Header />
       <main
         css={css`
-          margin: 2rem auto 4rem;
-          max-width: 90vw;
-          width: 550px;
+          margin: 2rem auto;
+          max-width: 550px;
         `}
       >
         {children}
