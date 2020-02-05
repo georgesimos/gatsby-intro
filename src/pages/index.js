7e27b013b2dbec1b48b8a3d688ce42dcd61aab3a
George <admin@georgesimos.com>
2020-02-05 20:12:26 +0200

    adding hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,21 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Gatsby</p>
-      <Link to="/about">Learn about me &rarr;</Link>
-
-      <h2>Read My Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-        // <pre>{JSON.stringify(post, null, 2)}</pre>
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read My Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+          // <pre>{JSON.stringify(post, null, 2)}</pre>
+        ))}
+      </Layout>
+    </>
   );
 };
