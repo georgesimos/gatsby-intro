745818986c2de40cb359bcbf37ee1aeee0dbb38a
George <admin@georgesimos.com>
2020-02-05 20:52:43 +0200

    hero :hammer:

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const ImageBackground = styled('div')`
-  background-image: url('/images/casey-horner.jpg');
-  bbackground-position: top 20% center;
+const ImageBackground = styled(BackgroundImage)`
+  background-position: top 20% center;
   background-size: cover;
   height: 50vh;
+  /* override the default margin for sibling elements  */
   + * {
     margin-top: 0;
   }
@@ -19,31 +20,41 @@ const TextBox = styled('div')`
   height: 100%;
   justify-content: flex-end;
   padding: 0 calc((100vw - 550px) / 2) 2rem;
+  margin-top: 0;
   width: 100%;
-
   h1 {
-    text-shadow: 1px 1px 3pxx #eeddff66;
+    text-shadow: 1px 1px 3px #eeddff66;
     font-size: 2.25rem;
   }
-
   p,
   a {
     color: #222;
     margin-top: 0;
   }
-
   a {
     margin-top: 0.5rem;
   }
 `;
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "casey-horner.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <ImageBackground>
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
       <TextBox>
         <h1>Frontend Masters + Gatsby &hearts;</h1>
         <p>
-          Hello Everyone <Link to="/about">Learn about me &rarr;</Link>
+          Hello Everyone! <Link to="/about/">Learn about me &rarr;</Link>
         </p>
       </TextBox>
     </ImageBackground>
